211f5efc128be1a892f7ba9ec08daa9b43f0558e
Biswajit Swain <biswajitswain552@gmail.com>
2024-04-10 19:34:23 +0530

    video modify

--- src/components/layout/Hero.js
+++ src/components/layout/Hero.js
@@ -4,24 +4,24 @@ import Right from "../icons/Right";
 
 export default function Hero() {
   return (
-    <section className="mt-5 md:mt-10 scroll-smooth h-[640px]   md:h-[650px]  ">
+    <section className="mt-5 md:mt-10 scroll-smooth h-[710px]   md:h-[650px]  ">
       {/* <img src={"/laptop.jpg"} className="md:hidden fixed -z-10" alt="" /> */}
       <video
         autoPlay
         loop
-        className=" md:h-[710px] h-[650px]  w-full -z-10 fixed object-cover"
+        className=" md:h-[710px] h-[710px]  w-full -z-10 fixed object-cover"
       >
         <source src={"/hero2.mp4"} type="video/mp4" />
       </video>
       <div className=" md:py-5  flex flex-col  ">
-        <h1 className="hero-heading mt-[260px] ml-3  md:mx-6 text-xl font-serif md:text-5xl    md:mt-10 font-semibold space-x-3  ">
+        <h1 className="hero-heading mt-[290px] ml-3  md:mx-6 text-xl font-serif md:text-5xl    md:mt-10 font-semibold space-x-3  ">
           Everything
           <br />
           is better with A&nbsp;
           <span className="text-primary">Technology</span>
         </h1>
 
-        <p className="my-5 mt-[150px] text-gray-800 md:text-2xl font-mono font-semibold text-center">
+        <p className="my-5 mt-[165px] text-gray-800 md:text-2xl font-mono font-semibold text-center">
           <span className="text-primary md:text-3xl font-serif">
             Technology
           </span>
